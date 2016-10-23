2bf6e5724fc65d02f17769279a5fb8f60a7e32a8
AlecJY <ae40515@yahoo.com.tw>
2016-10-23 09:28:13 +0800

    Rename assembly name and delete RsaKeygen

--- SymonClient/ClientMain.cs
+++ SymonClient/ClientMain.cs
@@ -4,10 +4,10 @@ namespace Symon.Client {
     class ClientMain {
         public ClientMain(string[] args) {
             ServicePointManager.ServerCertificateValidationCallback += (o, c, ch, er) => true;
-            // Broadcast broadcast = new Broadcast();
-            // string ip = broadcast.Listen();
+            Broadcast broadcast = new Broadcast();
+            string ip = broadcast.Listen();
             TcpStream stream = new TcpStream(PublicKeyReader.Read("key.cer"));
-            stream.Start("127.0.0.1");
+            stream.Start(ip);
         }
 
         static void Main(string[] args) {
