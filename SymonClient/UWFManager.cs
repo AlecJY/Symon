fa089aefeaf35d0126994d2507351e2bb4ce0b85
AlecJY <ae40515@yahoo.com.tw>
2017-01-11 09:19:49 +0800

    Add cim for connecting uwf

---
+++ SymonClient/UWFManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using Microsoft.Management.Infrastructure;
+
+namespace Symon.Client {
+    class UWFManager {
+        public UWFManager() {
+            CimSession cimSession = null;
+            CimInstance cimInstance;
+
+            CimInstance cimInstanceClass;
+
+            try {
+                if (cimSession != null) {
+                    cimSession.Close();
+                }
+                cimSession = CimSession.Create("127.0.0.1");
+                Thread.Sleep(2000);
+            }
+            catch (CimException e) {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            try {
+                cimInstanceClass = new CimInstance("UWF_Overlay");
+
+                cimInstance = cimSession.GetInstance(@"root\standardcimv2\embedded", cimInstanceClass);
+
+
+            }
+            catch (Exception e) {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
